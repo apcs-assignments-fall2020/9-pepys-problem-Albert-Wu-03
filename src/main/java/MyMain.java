219c17560d057a9dea78ca35f5827567574a0a2e
Albert-Wu-03 <22qwu@athenian.org>
2020-10-03 02:36:34 +0000

    Pepys problem

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class MyMain {
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -5,26 +6,83 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
+        int[] arr = new int[10000];
+        int n = 0;
+        int s = 0;        
+        double total = 0;
+        for(int i=0;i<10000;i++){
+            for(int j=0;j<6;j++){
+            	n = ThreadLocalRandom.current().nextInt(1, 7);
+            	if(n==6){
+            		s=1;
+            		break;
+            	}else{
+            		s=0;
+            	}            	
+            }
+            arr[i]=s;
+        }
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+        double average = (total / arr.length)*100;
+        return average;
+    }     
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
+        int[] arr = new int[10000];		
+        double total = 0;
+        for(int i=0;i<10000;i++){
+        	int s = 0;       
+            for(int j=0;j<12;j++){
+            	int d = ThreadLocalRandom.current().nextInt(1, 7);
+            	if(d==6) {
+            		s++;
+            	}
+            	if(s==2) {
+            		arr[i]+=1;
+            		break;
+            	}
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+        double average = total / arr.length*100;
+        return average;
+}
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int[] arr = new int[10000];
+        double total = 0;
+        for(int i=0;i<10000;i++){
+        	int s = 0;       
+            for(int j=0;j<18;j++){
+            	int d = ThreadLocalRandom.current().nextInt(1, 7);
+            	if(d==6) {
+            		s++;
+            	}
+            	if(s==3) {
+            		arr[i]+=1;
+            		break;
+            	}
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+        double average = total / arr.length*100;
+        return average;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(probabilityOneSix());
+        System.out.println(probabilityTwoSixes());
+        System.out.println(probabilityThreeSixes());
     }
 }
